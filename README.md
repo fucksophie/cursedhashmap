c92c8d1dc8c2db2fa888f3bf5fc5a1b2f296219b
yourfriendoss <81387799+yourfriendoss@users.noreply.github.com>
2024-02-17 10:44:44 +0200

    add hashmap_remove

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # cursedhashmap
 
 Cursed hashmap in C. Pretty simple to use, this isn't written as library though. You'll have to make it into a library. I also don't know if this leaks memory, but it doesn't seem like it should. I freed it correctly.
+
--- main.c
+++ main.c
@@ -9,6 +9,7 @@ typedef void* any_t;
 typedef struct hashmap_key {
     uint32_t key;
     any_t value;
+    int in_use;
 } hashmap_key;
 
 typedef struct hashmap {
@@ -19,7 +20,7 @@ typedef struct hashmap {
 void hashmap_grow(hashmap* hs, int growAmount) {
     if(!hs->size) {
         hs->size = growAmount;
-        hs->keys = (hashmap_key*)malloc(hs->size*sizeof(hashmap_key));
+        hs->keys = malloc(hs->size*sizeof(hashmap_key));
     } else {
         hs->size = hs->size + growAmount;
         hs->keys = realloc(hs->keys, hs->size*sizeof(hashmap_key));
@@ -42,9 +43,11 @@ any_t hashmap_get_value(hashmap* hs, char* key) {
     while(i < hs->size) {
         hashmap_key hkey = hs->keys[i];
         if(*(uint16_t *)&hkey != 0) {
-            if(hkey.key == hashedKey) {
-                slot = i;
-                break;
+            if(hkey.in_use == 1) {
+                if(hkey.key == hashedKey) {
+                    slot = i;
+                    break;
+                }
             }
         }
         i++;
@@ -56,6 +59,36 @@ any_t hashmap_get_value(hashmap* hs, char* key) {
         return hs->keys[slot].value;
     }
 }
+
+int hashmap_remove(hashmap* hs, char* key) {
+    int i = 0;
+    int slot = -1;
+    uint32_t hashedKey = murmurhash(key, strlen(key), 69);
+    while(i < hs->size) {
+        hashmap_key hkey = hs->keys[i];
+        if(*(uint16_t *)&hkey != 0) {
+            if(hkey.in_use == 1) {
+                if(hkey.key == hashedKey) {
+                    slot = i;
+                    break;
+                }
+            }
+        }
+        i++;
+    }
+
+    if(slot == -1) {
+        return 0;
+    } else {
+        hashmap_key key = hs->keys[slot];
+        key.in_use = 0;
+        key.key = 0;
+        key.value = NULL;
+        hs->keys[slot] = key;
+        return 1;
+    }
+}
+
 void hashmap_set_value(hashmap* hs, char* key, any_t value) {
     int i = 0;
     int freeKeySlot = -1;
@@ -70,9 +103,15 @@ void hashmap_set_value(hashmap* hs, char* key, any_t value) {
                 freeKeySlot = i;  
             }
         } else {
-            if(hkey.key == hashedKey) {
-                correctKey = i;
-                break;
+            if(hkey.in_use) {
+                if(hkey.key == hashedKey) {
+                    correctKey = i;
+                    break;
+                }
+            } else {
+                if(freeKeySlot == -1) {
+                    freeKeySlot = i;
+                }
             }
         }
         i++;
@@ -85,6 +124,7 @@ void hashmap_set_value(hashmap* hs, char* key, any_t value) {
             struct hashmap_key mkey;
             mkey.key = hashedKey;
             mkey.value = value;
+            mkey.in_use = 1;
             hs->keys[freeKeySlot] = mkey;
         }
     } else {
@@ -96,10 +136,16 @@ int main() {
     hashmap hs;
     hashmap_init(&hs);
 
+    // hashmap_set_value & hashmap_get_value
     hashmap_set_value(&hs, "Hello", "World");
-    
     printf("Hello %s!\n", hashmap_get_value(&hs, "Hello"));
     
+    // hashmap_remove
+    hashmap_set_value(&hs, "nooo", "Don't delete me!");
+    printf("Nooo! %s\n", hashmap_get_value(&hs, "nooo"));
+    hashmap_remove(&hs, "nooo");
+    printf("Nooo! %s\n", hashmap_get_value(&hs, "nooo"));
+
     hashmap_free(&hs);
     return 0;
 }
